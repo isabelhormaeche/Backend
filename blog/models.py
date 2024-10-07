1d419e091d90ba8020060ac3fdcb678d7ae0c44b
Isabel Hormaeche <isabel.hormaeche@gmail.com>
2024-10-08 00:41:23 +0200

    Created models file and created draft blog table

---
+++ blog/models.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from .database import Base
+from sqlalchemy.orm import relationship
+
+
+class Blog(Base):
+    __tablename__ = 'blogs'
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    desc = Column(String)
+    cat = Column(String)
+    
